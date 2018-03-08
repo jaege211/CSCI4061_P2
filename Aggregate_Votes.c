add39dbbd3639dfcaafb2e39deaf0eadf1e46592
jaege211 <jaege211@umn.edu>
2018-03-08 14:10:33 -0600

    got it closer to working

--- Aggregate_Votes.c
+++ Aggregate_Votes.c
@@ -8,26 +8,68 @@
 #include <unistd.h>
 #include <string.h>
 
-#ifndef MAX_BUF
 #define MAX_BUF 1024
-#endif
+#define _BSD_SOURCE
+
+typedef struct person {
+	char* name;
+	int votes;
+} person_t;
+
+// add person to last index in election array
+int addToElection(char* name, person_t* election)
+{
+	int i = 0;
+	while (election[i].name != NULL) i++;
+
+	election[i].name = name;
+	election[i].votes = 0;
+
+	return i;
+}
+
+// check if a person is in the election
+int checkInElection(char* name, person_t* election)
+{
+	int i = 0;
+	while (election[i].name != NULL)
+	{
+		if (!strcmp(election[i].name, name)) return 1;
+		i++;
+	}
+
+	return 0;
+}
+
+// find person in election by name
+int findIndexByName(char* name, person_t* election)
+{
+	int i = 0;
+	while (election[i].name != NULL)
+	{
+		if (!strcmp(election[i].name, name)) return i;
+		i++;
+	}
+
+	return -1;
+}
 
 // check if directory is a leaf
 int checkIsLeaf(DIR *pDir, struct dirent *pEntry)
 {
 	rewinddir(pDir);
 
-	while ((pEntry = readdir(pDir)) != NULL) 
+	while ((pEntry = readdir(pDir)) != NULL)
 	{
-		if (pEntry->d_type == DT_DIR && strcmp(pEntry->d_name, ".") != 0 && strcmp(pEntry->d_name, "..") != 0) 
+		if (pEntry->d_type == DT_DIR && strcmp(pEntry->d_name, ".") != 0 && strcmp(pEntry->d_name, "..") != 0)
 			return 0;
 	}
-	
+
 	return 1;
 }
 
 // call Aggregate_Votes on all child directories
-int execOnChildren(DIR *pDir, struct dirent *pEntry) 
+int execOnChildren(DIR *pDir, struct dirent *pEntry)
 {
 	rewinddir(pDir);
 
@@ -35,22 +77,13 @@ int execOnChildren(DIR *pDir, struct dirent *pEntry)
 	int status[MAX_BUF];
 	int i= -1;
 
-	char *cwd = (char *) malloc(sizeof(char *) * MAX_BUF);
-
-	while ((pEntry = readdir(pDir)) != NULL) 
+	while ((pEntry = readdir(pDir)) != NULL)
 	{
-		if (pEntry->d_type == DT_DIR && strcmp(pEntry->d_name, ".") != 0 && strcmp(pEntry->d_name, "..") != 0) 
+		if (pEntry->d_type == DT_DIR && strcmp(pEntry->d_name, ".") != 0 && strcmp(pEntry->d_name, "..") != 0)
 		{
-			if (chdir(pEntry->d_name)) 
-			{
-				perror(pEntry->d_name);
-				return 1;
-			}
-
 			pid[++i] = fork();
 
 			if (pid[i] == 0) { // child
-				chdir("..");
 				execlp("Aggregate_Votes", "Aggregate_Votes", pEntry->d_name, (char *) NULL);
 			} else if (pid[i] > 0) { // parent
 				waitpid(pid[i], &status[i], 0);
@@ -59,19 +92,15 @@ int execOnChildren(DIR *pDir, struct dirent *pEntry)
 				perror("Fork error");
 				return 1;
 			}
-
-			chdir("..");
 		}
 	}
-
-	free(cwd);
 
 	return 0;
 }
 
 int main(int argc, char const *argv[])
 {
-	if (argc != 2) 
+	if (argc != 2)
 	{
 		printf("Incorrect number of arguments, Expected 1 but %d entered\n", argc - 1);
 		return 1;
@@ -105,21 +134,26 @@ int main(int argc, char const *argv[])
 	if (checkIsLeaf(pDir, pEntry)) // execute leaf_counter if the directory is a leaf
 	{
 		chdir("..");
-		char cwd[MAX_BUF];
-		getcwd(cwd, MAX_BUF);
-		printf("%s\n", cwd);
-		printf("Passing %s to Leaf_Counter\n", argv[1]);
 		closedir(pDir);
 		execlp("Leaf_Counter", "Leaf_Counter", argv[1], (char *) NULL);
-		return 0;
 	}
 
 	execOnChildren(pDir, pEntry);
 
-	// Execute vote counter on outputs of Leaf_Counter
-	printf("--- %s is done ---\n", argv[1]);
+	char cwd[MAX_BUF];
+	getcwd(cwd, MAX_BUF);
+	printf("%s\n", cwd);
 
+	person_t election[MAX_BUF];
 
+	char str[] = "A:1,B:1,D:4,C:6";
+	char* tok;
+	tok = strtok(str, ",");
+	printf("%s\n", tok);
+
+	while ((tok = strtok(NULL, ",")) != NULL) {
+		printf("%s\n", tok);
+	}
 
 	closedir(pDir);
 
